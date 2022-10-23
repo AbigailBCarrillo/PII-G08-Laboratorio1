e3028fd32adf4affd15bad95f27f5bd632442f22
AbigailBCarrillo <106496635+AbigailBCarrillo@users.noreply.github.com>
2022-10-23 12:24:06 -0500

    adding data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const clientes = [
+    { id: 1, dni: "12345678", apellidos: "Pardo Aguilar", nombres: "Ana María", edad: 52 },
+    { id: 2, dni: "23456789", apellidos: "Espinoza Farías", nombres: "Pedro Ramón", edad: 23 },
+    { id: 3, dni: "90087654", apellidos: "Gonzales Ramos", nombres: "Laura Daniela", edad: 19 },
+    { id: 4, dni: "50679551", apellidos: "Ramírez Guerra", nombres: "José María", edad: 35 },
+    { id: 5, dni: "36457009", apellidos: "García Duarte", nombres: "Ariana Mercedes", edad: 28 },
+    { id: 6, dni: "64820182", apellidos: "Farfán Rodríguez", nombres: "Julio Armando", edad: 22 },
+    { id: 7, dni: "79843050", apellidos: "Guerrero Camargo", nombres: "Sara Grace", edad: 46 },
+    { id: 8, dni: "44820163", apellidos: "Jiménez Suárez", nombres: "David Esteban", edad: 37 },
+    { id: 9, dni: "84196231", apellidos: "Meléndez Uribe", nombres: "Fiorella Alicia", edad: 20 },
+    { id: 10, dni: "07845626", apellidos: "Torres Huamán", nombres: "Carlos Fabián", edad: 54 }
+]
+
+module.exports = {clientes}
